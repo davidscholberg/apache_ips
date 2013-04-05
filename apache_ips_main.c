108949ac7aa104e57f4898a6d74f698fc0f7d94c
David Scholberg <recombinant.vector@gmail.com>
2013-04-05 11:06:48 -0500

    Added copyright notices and license

--- apache_ips_main.c
+++ apache_ips_main.c
@@ -1,7 +1,20 @@
 /*
-David Scholberg
-Master's Project
-Fall 2012
+    Copyright 2013 David Scholberg <recombinant.vector@gmail.com>
+
+    This file is part of apache_ips.
+
+    apache_ips is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    apache_ips is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with apache_ips.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**********
